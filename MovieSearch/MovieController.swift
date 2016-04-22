070ff42455314c5b8eb271cff376345a3555fb54
Parker Donat <parkerdonat@gmail.com>
2016-04-22 14:59:18 -0600

    Created MovieController

---
+++ MovieSearch/MovieController.swift
@@ -0,0 +1,36 @@
+//
+//  MovieController.swift
+//  MovieSearch
+//
+//  Created by Parker Donat on 4/22/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class MovieController {
+    
+    static func searchForMovies(searchString: String, completion: (movieArray: [Movie]) -> Void) {
+        
+        let key = "9730fb5307b38c315c5b99aa52f32f31"
+        let base = "https://api.themoviedb.org/3/movie"
+        let modifiedSearchTerm = searchString.stringByReplacingOccurrencesOfString(" ", withString: "+")
+
+        let url = base + "?query=\(modifiedSearchTerm)" + "&api_key=\(key)"
+        
+        NetworkController.dataAtURL(url) { (success, data) in
+            
+            guard let data = data,
+            json = NetworkController.serializeDataWithReturn(data),
+            movieArray = json["results"] as? [[String: AnyObject]] where success
+             else { completion(movieArray: []); return }
+            
+            let movies = movieArray.flatMap{ Movie(dictionary: $0)}
+            completion(movieArray: movies)
+            }
+    }
+    
+    static func posterURLForString(movie: Movie) -> String {
+        return "http://image.tmdb.org/t/p/w500/\(movie.posterEndpoint).jpg"
+    }
+}
